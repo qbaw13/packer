57f9fd633aaaa9098a73c117ca690b024f3ff60d
qbaw13 <qbawojtowicz@gmail.com>
2016-05-24 13:48:15 +0200

    changes in stub

--- src/stub/PackResultStub.java
+++ src/stub/PackResultStub.java
@@ -18,8 +18,8 @@ public class PackResultStub {
     static {
         List<Shape> shapes = new ArrayList<>();
         shapes.add(new Rectangle(0, 0, 100, 100));
-        shapes.add(new Rectangle(100, 0, 50, 50));
-        shapes.add(new Rectangle(150, 0, 20, 20));
+        shapes.add(new Rectangle(100, 0, 50, 80));
+        shapes.add(new Rectangle(150, 0, 30, 70));
 //        shapes.add(new Rectangle(100, 0, 20, 100));
 //        shapes.add(new Rectangle(100, 20, 70, 70));
 //        shapes.add(new Rectangle(100, 100, 100, 100));
